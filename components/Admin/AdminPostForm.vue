f0989825fccac21f4f71fa24ab2864ace2de7254
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 17:12:31 +0430

    Admit NewPost Form Removed To Admin Component

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -1,5 +1,18 @@
 <template>
-  
+  <form @submit.prevent="onSave">
+    <UIAppControlInput v-model="editedPost.author">Author Name</UIAppControlInput>
+    <UIAppControlInput v-model="editedPost.title">Title</UIAppControlInput>
+    <UIAppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</UIAppControlInput>
+    <UIAppControlInput
+      control-type="textarea"
+      v-model="editedPost.content">Content</UIAppControlInput>
+    <UIAppButton type="submit">Save</UIAppButton>
+    <UIAppButton
+      type="button"
+      style="margin-left: 10px"
+      btn-style="cancel"
+      @click="onCancel">Cancel</UIAppButton>
+  </form>
 </template>
 
 <script>
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="admin-new-post-page">
     <sectoin class="new-post-form">
-      <form @submit.prevent="onSave">
-        <UIAppControlInput v-model="editedPost.author">Author Name</UIAppControlInput>
-        <UIAppControlInput v-model="editedPost.title">Title</UIAppControlInput>
-        <UIAppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</UIAppControlInput>
-        <UIAppControlInput
-          control-type="textarea"
-          v-model="editedPost.content">Content</UIAppControlInput>
-        <UIAppButton type="submit">Save</UIAppButton>
-        <UIAppButton
-          type="button"
-          style="margin-left: 10px"
-          btn-style="cancel"
-          @click="onCancel">Cancel</UIAppButton>
-      </form>
+
     </sectoin>
   </div>
 </template>
