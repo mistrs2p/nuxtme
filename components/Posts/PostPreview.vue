a793e87a34ef76e935a55504ed756675a7315e2f
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 17:56:08 +0430

    PostLink Computed Property Added

--- components/Posts/PostPreview.vue
+++ components/Posts/PostPreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <nuxt-link :to="'/posts/' + id" class="post-preview">
+  <nuxt-link :to="postLink" class="post-preview">
     <article>
       <div 
       class="post-thumbnail" 
@@ -36,6 +36,11 @@ export default {
       type: String,
       required: true
     },
+  },
+  computed: {
+    postLink() {
+      return this.isAdmin ? '/admin/' + this.id : '/posts/' + this.id
+    }
   }
 }
 </script>
