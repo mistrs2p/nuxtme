901282c4d7f40e9296eaabb3ade5c46558c99536
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 14:14:47 +0430

    Posts Subfolder _id with Structure&Template Added

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1>Title of the post ID: {{ $route.params.id }}</h1>
+      <div class="post-details">
+        <div>Last updated on XXX</div>        
+        <div>Written by NAME</div>        
+      </div>
+      <p>Content of the post</p>
+    </section>
+    <section class="post-feedback">
+      <p>Let me know what you think about the post, sent a mail to <a href="mailto:feedback@my-awesome-domain.com">feedback@my-awesome-domain.com</a>.</p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
