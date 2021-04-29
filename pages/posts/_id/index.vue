1b706e17e7fbeddfeadc58ae8df1137d7891a99c
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 14:19:02 +0430

    Posts Subfolder _id Style Added

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="single-post-page">
     <section class="post">
-      <h1>Title of the post ID: {{ $route.params.id }}</h1>
+      <h1 class="post-title">Title of the post ID: {{ $route.params.id }}</h1>
       <div class="post-details">
-        <div>Last updated on XXX</div>        
-        <div>Written by NAME</div>        
+        <div class="post-detail">Last updated on XXX</div>        
+        <div class="post-detail">Written by NAME</div>        
       </div>
-      <p>Content of the post</p>
+      <p class="post-content">Content of the post</p>
     </section>
     <section class="post-feedback">
       <p>Let me know what you think about the post, sent a mail to <a href="mailto:feedback@my-awesome-domain.com">feedback@my-awesome-domain.com</a>.</p>
@@ -20,6 +20,56 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .single-post-page {
+    padding: 30px;
+    text-align: center;
+    box-sizing: border-box;
+  }
 
+  .post {
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    .post {
+      width: 600px;
+      margin: auto;
+    }
+  }
+
+  .post-title {
+    margin: 0;
+  }
+
+  .post-details {
+    padding: 10px;
+    box-sizing: border-box;
+    border-bottom: 3px solid #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  @media (min-width: 768px) {
+    .post-details {
+      flex-direction: row;
+    }
+  }
+
+  .post-detail {
+    color: rgb(88, 88, 88);
+    margin: 0 10px;
+  }
+
+  .post-feedback a {
+    color: red;
+    text-decoration: none;
+  }
+
+  .post-feedback a:hover,
+  .post-feedback a:active {
+    color: salmon;
+  }
 </style>
