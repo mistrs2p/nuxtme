5aa90ad40969da8e3dfd748e4ffc77b0aee7d6e1
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 15:52:52 +0430

    PostList Component Added And Included

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,7 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostsPostPreview
-        id="1"
-        thumbnail="https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-        title="Hello There"
-        previewText="This is my first post" />
-      <PostsPostPreview
-        id="2"
-        thumbnail="https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-        title="Hi Here"
-        previewText="This is my Second post" />      
-      <PostsPostPreview
-        id="3"
-        thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF-GzPP9Za0RMIcDruDbTgF25fKvEsTlbIWg&usqp=CAU"
-        title="By Now"
-        previewText="This is my Third post" />      
-      
-    </section>
+    <PostsPostList />
   </div>
 </template>
 
@@ -59,15 +42,4 @@ export default {}
       font-size: 2rem;
     }
   }
-
-  .featured-posts {
-    display: flex;
-    padding: 20px;
-    box-sizing: border-box;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-  }
-
-  
 </style>
