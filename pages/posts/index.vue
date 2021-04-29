5347ebc716c0d80b028ac95845ccc3e23b3cc598
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 14:08:13 +0430

    Posts Page Refactored And Recode Correctly And About Page Structure Added

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="about-page">
-    <h1>This is my absolutely fantastic "About Me" Page!</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
-  </div>
+  <div class="posts-page"></div>
 </template>
 
 <script>
@@ -15,8 +9,9 @@ export default {
 </script>
 
 <style scoped>
-.about-page {
-  text-align: center;
-  padding: 30px;
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
