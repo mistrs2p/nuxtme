179f91afc6cfb34a4846c3548a7d70d3062b2199
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 17:49:44 +0430

    Props & Bind Props Added To PostList Component

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,16 +2,19 @@
   <section class="posts-list">
     <PostsPostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
       title="Hello There"
       previewText="This is my first post" />
     <PostsPostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
       title="Hi Here"
       previewText="This is my Second post" />      
     <PostsPostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF-GzPP9Za0RMIcDruDbTgF25fKvEsTlbIWg&usqp=CAU"
       title="By Now"
       previewText="This is my Third post" />      
@@ -20,7 +23,12 @@
 
 <script>
 export default {
-
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }    
+  }
 }
 </script>
 
