bbb0072e20cb41e26153803442bb3f9e09179f8d
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 14:04:03 +0430

    About Page Structure with Style Added

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <div class="about-page">
+    <h1>This is my absolutely fantastic "About Me" Page!</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam officiis ab voluptatibus assumenda error quas deleniti eius? Suscipit, numquam? Quis aliquid non animi numquam amet voluptas dolorum sunt aspernatur commodi?</p>
+  </div>
 </template>
 
 <script>
@@ -8,6 +14,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
 </style>
