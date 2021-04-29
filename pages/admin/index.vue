2e44ada471265af11e2abd5d3f769aac2bbcec54
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 16:27:52 +0430

    Admin Index Page Structure Added&Styled

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <div class="admin-page">
+    <section class="new-post">
+      <button>Create Post</button>
+    </section>
+    <section class="existing-posts">
+      <h1>Existing post</h1>
+      <PostsPostList />
+    </section>
+  </div>
 </template>
 
 <script>
@@ -8,6 +16,18 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .admin-page {
+    padding: 20px;
+  }
 
+  .new-post {
+    text-align: center;
+    border-bottom: 2px solid #ccc;
+    padding-bottom: 10px;
+  }
+
+  .existing-posts h1 {
+    text-align: center;
+  }
 </style>
