0e362c752d97d03a8d0d2622a4325cbf6243a7bd
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-29 17:34:10 +0430

    Fake Data Added To Updata Form And Pass As props

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,10 +1,23 @@
 <template>
-  
+  <div class="admin-post-page">
+    <div class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      loadedPost: {
+        author: 'Mahdi',
+        title: 'Poste Man',
+        content: 'Mikham Parvaz Konam',
+        thumbnailLink: 'https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg'
+      }
+    }
+  }
 }
 </script>
 
