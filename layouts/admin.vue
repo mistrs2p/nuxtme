418eecc25f1524ef1ab39e2fd6e9c11c5773853b
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-04-30 17:35:36 +0430

    Admin Layout Added

---
+++ layouts/admin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      displaySidenav: false
+    }
+  }
+}
+</script>
+<style>
+html {
+  font-family: 'Ubuntu', sans-serif;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+</style>
