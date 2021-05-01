de01627245fd9a18e51383bdf4859ba3e35b9b8c
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-01 16:36:43 +0430

    Promise Approach Added For Rendering Server Side

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -13,37 +13,46 @@ export default {
       setTimeout(() => this.$nuxt.$loading.finish(), 500)
     })
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: "1",
-            title: "First Post",
-            previewText: "This is First mYPost",
-            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-          },
-          {
-            id: "2",
-            title: "Second Post",
-            previewText: "This is Second2 mYPost",
-            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-          },
-          {
-            id: "3",
-            title: "Third Post",
-            previewText: "This is 3threeee mYPost",
-            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-          },
-          {
-            id: "4",
-            title: "Forth4 Post",
-            previewText: "This is 4&4 mYPost",
-            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-          }
-        ]
-      })
-    }, 1500)
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "This is First mYPost",
+              thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is Second2 mYPost",
+              thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+            },
+            {
+              id: "3",
+              title: "Third Post",
+              previewText: "This is 3threeee mYPost",
+              thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+            },
+            {
+              id: "4",
+              title: "Forth4 Post",
+              previewText: "This is 4&4 mYPost",
+              thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+            }
+          ]
+        })
+      }, 1500)
+      // reject(new Error)
+    })
+    .then(data => {
+      return data
+    })
+    .catch(e => {
+      context.error(e)
+    })
   },
 }
 </script>
