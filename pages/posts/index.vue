9831c8ccddd920496bfdd8b39f8730bbc76a839a
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-01 17:27:37 +0430

    Fetch Promise Removed To Nuxt Server Actuin Store And Configured

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -13,48 +13,6 @@ export default {
       setTimeout(() => this.$nuxt.$loading.finish(), 500)
     })
   },
-  fetch(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: "1",
-              title: "First Post",
-              previewText: "This is First mYPost",
-              thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-            },
-            {
-              id: "2",
-              title: "Second Post",
-              previewText: "This is Second2 mYPost",
-              thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-            },
-            {
-              id: "3",
-              title: "Third Post",
-              previewText: "This is 3threeee mYPost",
-              thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-            },
-            {
-              id: "4",
-              title: "Forth4 Post",
-              previewText: "This is 4&4 mYPost",
-              thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-            }
-          ]
-        })
-      }, 1500)
-      // reject(new Error)
-    })
-    .then(data => {
-      // return data
-      context.store.commit('setPosts', data.loadedPosts)
-    })
-    .catch(e => {
-      context.error(e)
-    })
-  },
   computed: {
     loadedPosts() {
       return this.$store.getters.loadedPosts
--- store/index.js
+++ store/index.js
@@ -11,7 +11,40 @@ const createStore = () => {
       }
     },
     actions: {
-      nuxtServerInit(vuexContext, context) {},
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit('setPosts', [
+              {
+                id: "1",
+                title: "First Post",
+                previewText: "This is First mYPost",
+                thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+              },
+              {
+                id: "2",
+                title: "Second Post",
+                previewText: "This is Second2 mYPost",
+                thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+              },
+              {
+                id: "3",
+                title: "Third Post",
+                previewText: "This is 3threeee mYPost",
+                thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+              },
+              {
+                id: "4",
+                title: "Forth4 Post",
+                previewText: "This is 4&4 mYPost",
+                thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+              }
+            ])
+            resolve()
+          }, 1500)
+          // reject(new Error)
+        })
+      },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts)
       }
