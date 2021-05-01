819124e2c5749a206a05fe8fd9607e929701cc2d
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-01 15:41:35 +0430

    Fake Data Props Added To Blog Page

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostsPostList />
+    <PostsPostList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -12,7 +12,39 @@ export default {
 
       setTimeout(() => this.$nuxt.$loading.finish(), 500)
     })
-  }
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is First mYPost",
+            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is Second2 mYPost",
+            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+          },
+          {
+            id: "3",
+            title: "Third Post",
+            previewText: "This is 3threeee mYPost",
+            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+          },
+          {
+            id: "4",
+            title: "Forth4 Post",
+            previewText: "This is 4&4 mYPost",
+            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+          }
+        ]
+      })
+    }, 1500)
+  },
 }
 </script>
 
