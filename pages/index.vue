d3de6f1a1e9be10e08329b9ff83cc0783ec8b59b
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-01 15:33:03 +0430

    AsyncData Added To Server Side Render Posts

--- pages/index.vue
+++ pages/index.vue
@@ -16,36 +16,43 @@ export default {
       setTimeout(() => this.$nuxt.$loading.finish(), 500)
     })
   },
-  data() {
-    return {
-      loadedPosts: [
-        {
-          id: "1",
-          title: "First Post",
-          previewText: "This is First mYPost",
-          thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-        },
-        {
-          id: "2",
-          title: "Second Post",
-          previewText: "This is Second2 mYPost",
-          thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-        },
-        {
-          id: "3",
-          title: "Third Post",
-          previewText: "This is 3threeee mYPost",
-          thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-        },
-        {
-          id: "4",
-          title: "Forth4 Post",
-          previewText: "This is 4&4 mYPost",
-          thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
-        }
-      ]
-    }
-  }
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is First mYPost",
+            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is Second2 mYPost",
+            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+          },
+          {
+            id: "3",
+            title: "Third Post",
+            previewText: "This is 3threeee mYPost",
+            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+          },
+          {
+            id: "4",
+            title: "Forth4 Post",
+            previewText: "This is 4&4 mYPost",
+            thumbnail: "https://www.dti.dk/_/media/73504&w=1460&h=808&r=cover&_filename=73504_Web_tech-mining.jpg"
+          }
+        ]
+      })
+    }, 1500)
+  },
+  // data() {
+  //   return {
+      
+  //   }
+  // }
 }
 </script>
 
